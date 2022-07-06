68a2ace7a7031e11b280c7b6cef2f644327535b6
shiryu <salvador.pendon@axartel.es>
2022-07-06 12:54:59 +0200

    actualicion incluir eureka

--- spring-eureka-service/src/main/java/com/microservicios/SpringEurekaServiceApplication.java
+++ spring-eureka-service/src/main/java/com/microservicios/SpringEurekaServiceApplication.java
@@ -2,14 +2,14 @@ package com.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class SpringCarServiceApplication {
+@EnableEurekaServer
+public class SpringEurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCarServiceApplication.class, args);
+		SpringApplication.run(SpringEurekaServiceApplication.class, args);
 	}
 
 }
---
+++ spring-eureka-service/src/test/java/com/microservicios/SpringEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

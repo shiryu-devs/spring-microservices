17e05d0cbe278b18090094ebfdd0e2e6f24d91b0
shiryu <salvador.pendon@axartel.es>
2022-07-07 18:14:08 +0200

    actualizacion de autentificacion con keycloak

--- spring-user-service/src/main/java/com/microservicios/security/UserSecurity.java
+++ spring-user-service/src/main/java/com/microservicios/security/UserSecurity.java
@@ -1,5 +1,37 @@
 package com.microservicios.security;
 
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserSecurity {
+
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+		super.configure(http);
+	}*/
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+        	.anyRequest()
+        	.authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+        
+        return http.build();
+    }
 
 }
